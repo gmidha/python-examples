8e8b12ad41b13ed234fed11fd5661402de81cda4
gmidha <gaurav45750@gmail.com>
2018-09-05 21:44:23 -0400

    updating the file

--- sets_example.py
+++ sets_example.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+!/usr/bin/env python3
 
 def main():
     """  This program displays the usage of sets that stores unique items as
@@ -11,6 +11,13 @@ def main():
     print(f"Coworkers friends are: {coworkers}")
     print(f"Family friends are: {family}")
     print(f"All of my friends are: {college.union(coworkers, family)}")
+    college.add('Anurag')
+    print(f"College friends are: {college}")
+    college.remove('Ankit')
+    print(f"College friends are: {college}")
+    print(f"{college.pop()}")
+    print(f"{college.pop()}")
+    print(f"College friends are: {college}")
 
 if __name__ == '__main__':
     main()
